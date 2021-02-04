ffaea30b64b5b5606c1ca2dbc4cbbc025159fdf0
Artis3n <dev@artis3nal.com>
2021-02-03 23:11:14 -0500

    Setting up Gollum execution and repo syncing

---
+++ packer/ansible/files/backup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+eval '$(ssh-agent -s)'
+trap 'kill $SSH_AGENT_PID' EXIT
+ssh-add /home/wiki/.ssh/wiki_deploy_key
+cd /opt/wiki
+git pull origin main
+git push origin main
